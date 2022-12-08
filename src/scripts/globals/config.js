2e2c00490c7fff3db88454de86aaa6eeb55825c4
icalbalino <balino.ical@gmail.com>
2022-12-08 11:14:36 +0700

    random img generator

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,6 +1,7 @@
 const CONFIG = {
   BASE_URL: '',
   BASE_IMAGE_URL: (size) => `.../images/${size}/`,
+  BASE_CATEGORY_IMAGE_URL: (img) => `../../public/categoryimg/${img}`,
   CACHE_NAME: 'spendeeds_cache',
   DATABASE_NAME: 'spendeeds_db',
   DATABASE_VERSION: 1,
