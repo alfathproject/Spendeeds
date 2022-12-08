e0a943bf35be5aec132ca2627be8ce31ffdceec5
Hafizquran7 <insideme400@gmail.com>
2022-12-08 14:04:30 +0700

    upd our team section

---
+++ src/scripts/data/team-source.js
@@ -0,0 +1,9 @@
+import team from './team.json';
+
+class teamSource {
+    static async team() {
+        return team;
+    }
+}
+
+export default teamSource;
--- src/scripts/views/components/team-item.js
+++ src/scripts/views/components/team-item.js
@@ -1,5 +1,10 @@
 class TeamItem extends HTMLElement {
-  connectedCallback() {
+  /**
+   * @param {any} team
+   */
+  set team(team) {
+    this._team = team;
+
     this.render();
   }
 
@@ -7,18 +12,18 @@ class TeamItem extends HTMLElement {
     this.innerHTML = `
       <div class="team-item bg-light">
         <div class="overflow-hidden">
-          <img class="img-fluid" src="./img/team-1.jpg" alt="">
+          <img class="img-fluid" src="./img/${this._team.photo}" alt="">
         </div>
         <div class="position-relative d-flex justify-content-center" style="margin-top: -23px;">
           <div class="bg-light d-flex justify-content-center pt-2 px-1">
-            <a class="btn btn-sm-square btn-primary mx-1" href="#"><i class="bi bi-facebook"></i></a>
-            <a class="btn btn-sm-square btn-primary mx-1" href="#"><i class="bi bi-twitter"></i></a>
-            <a class="btn btn-sm-square btn-primary mx-1" href="#"><i class="bi bi-instagram"></i></a>
+            <a class="btn btn-sm-square btn-primary mx-1" href="https://web.facebook.com/${this._team.sosmed.facebook}" target="_blank"><i class="bi bi-facebook"></i></a>
+            <a class="btn btn-sm-square btn-primary mx-1" href="https://twitter.com/${this._team.sosmed.twitter}" target="_blank"><i class="bi bi-twitter"></i></a>
+            <a class="btn btn-sm-square btn-primary mx-1" href="https://www.instagram.com/${this._team.sosmed.instagram}" target="_blank"><i class="bi bi-instagram"></i></a>
           </div>
         </div>
         <div class="text-center p-4">
-          <h5 class="mb-0">DALIMAN, S.Pd</h5>
-          <small>Headmaster</small>
+          <h5 class="mb-0">${this._team.name}</h5>
+          <small>${this._team.job}</small>
         </div>
       </div>`;
   }
